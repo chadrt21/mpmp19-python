5651a1aca1c395a6ead8104e8b3174d1773430f1
Chad Ross <chadrt21@gmail.com>
2020-11-30 15:12:28 -0800

    update program & remove old data

--- mpmp19.py
+++ mpmp19.py
@@ -8,18 +8,17 @@ More info: http://www.think-maths.co.uk/19challenge
 
 __author__ = "Chad Ross"
 __email__ = "chadrt21@gmail.com"
-__date__ = "2020-11-27"
+__date__ = "2020-11-30"
 
 import numpy
 import sympy
-from datetime import datetime
 from tqdm import tqdm   # progress bar (may need to install package from: https://github.com/tqdm/tqdm)
 
-start = datetime.now()
 
-# Download & unzip prime numbers files from: https://primes.utm.edu/lists/small/millions/
-num_files = 50
-prime_files = ['primes{}.txt'.format(f) for f in range(1,num_files+1)]
+# Download & unzip prime numbers files from: http://www.primos.mat.br/2T_en.html
+# Each file contains 10 million primes (~101 MB) -> total of 2 billion primes (~20 GB)
+num_files = 200
+prime_files = ['2T_part{}.txt'.format(f) for f in range(1,num_files+1)]
 
 n_array = []
 
@@ -27,29 +26,22 @@ file_count = 0
 n_count = 1
 for c in tqdm(range(1,len(prime_files))):
 
-    primes = numpy.loadtxt(prime_files[file_count],skiprows=1)
+    primes = numpy.loadtxt(prime_files[file_count])
     primes = primes.flatten()
 
     primes_sq = numpy.power(primes,2)
     primes_cumsum = numpy.cumsum(primes_sq)
 
-    for n in range(1,1000000):
+    for n in range(1,len(primes)):
         if((primes_cumsum[n-1] % n_count) == 0):
-            # print('n:',n_count)
-            # print('primes:',primes[:n])
-            # print('primes_sq:',primes_sq[:n])
-            # print('sum primes_sq:',primes_cumsum[n-1])
-            # print('sum primes_sq / n:',primes_cumsum[n-1]/n)
-            # print('sum primes_sq % n:',primes_cumsum[n-1]%n)
             n_array.append(n_count)
         n_count += 1
 
     file_count += 1
 
 print()
-print("Durtation:",datetime.now()-start)
 print('n_array:',n_array)
 print('len(n_array):',len(n_array))
 print()
 print('max n:',f'{n_array[-1]:,}')
-numpy.savetxt('mpmp19_out.txt',n_array)
+numpy.savetxt('mpmp19_out.txt',n_array, fmt='%d',header='All n of the first {:,} primes'.format(n_count))
